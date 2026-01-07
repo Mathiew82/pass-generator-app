5243472c2a89bfc0f30e665ed1cb43a981cdbd67
Mathiew82 <amateo1982@yahoo.es>
2026-01-07 21:09:15 +0100

    feat: improve the logic to determine the security of the password

--- src/logic/feedback.rs
+++ src/logic/feedback.rs
@@ -1,13 +1,30 @@
 use crate::logic::state::PasswordOptions;
-use crate::ui::texts::{LOW_SECURITY, MEDIUM_SECURITY, HIGH_SECURITY};
+use crate::ui::texts::{HIGH_SECURITY, LOW_SECURITY, MEDIUM_SECURITY};
 
 pub fn display_info(opts: PasswordOptions) -> (&'static str, &'static str) {
-    let check_options: [bool; 4] = [opts.lowercase, opts.uppercase, opts.numbers, opts.symbols];
-    let selected_checks = check_options.into_iter().filter(|v| *v).count();
+    let mut score = 0;
+    if opts.length >= 8 {
+        score += 1;
+    }
+    if opts.length >= 12 {
+        score += 1;
+    }
+    if opts.lowercase {
+        score += 1;
+    }
+    if opts.uppercase {
+        score += 1;
+    }
+    if opts.numbers {
+        score += 1;
+    }
+    if opts.symbols {
+        score += 1;
+    }
 
-    if opts.length < 8 {
+    if score <= 2 {
         (LOW_SECURITY, "security-low")
-    } else if opts.length >= 8 && selected_checks < 3 {
+    } else if score <= 4 {
         (MEDIUM_SECURITY, "security-medium")
     } else {
         (HIGH_SECURITY, "security-high")
